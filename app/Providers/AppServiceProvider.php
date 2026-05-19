f4e4788a993625191cbe89fb577460e07983e86a
lenovo <ssmiyihhamal@gmail.com>
2026-05-19 12:48:02 +0300

    Fix url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
    public function boot(): void
 {
-  
+  if (config('app.env') === 'production' || env('APP_URL') !== 'http://localhost') {
+        URL::forceScheme('https');
+    }
 }
 }
